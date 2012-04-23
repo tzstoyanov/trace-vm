ce97e7242b43f510eacf6b46fa9cd5cac4c8be16
Namhyung Kim <namhyung.kim@lge.com>
2012-04-23 13:58:33 +0900

    parse-events: Pass string type argument to args
    It seems PEVENT_FUNC_ARG_STRING missed passing the allocated
    string to the args array. Fix it.
    
    Link: http://lkml.kernel.org/r/1335157118-14658-7-git-send-email-namhyung.kim@lge.com
    
    Signed-off-by: Namhyung Kim <namhyung.kim@lge.com>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Namhyung Kim <namhyung@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Arnaldo Carvalho de Melo <acme@infradead.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: David Ahern <dsahern@gmail.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- parse-events.c
+++ parse-events.c
@@ -3404,6 +3404,7 @@ process_defined_func(struct trace_seq *s, void *data, int size,
 			if (!string->str)
 				die("malloc str");
 
+			args[i] = (unsigned long long)string->str;
 			strings = string;
 			trace_seq_destroy(&str);
 			break;
