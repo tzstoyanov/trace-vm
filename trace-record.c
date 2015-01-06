d5c2a168fb8c558a63e28e6b3912b9e9c6c262a6
Steven Rostedt (Red Hat) <rostedt@goodmis.org>
2015-01-06 10:23:27 -0500

    trace-cmd record: Fix memory leak in check_plugin()
    valgrind discovered that check_plugin() was leaking the buffer that was
    used to parse tokens.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- trace-record.c
+++ trace-record.c
@@ -2844,6 +2844,7 @@ static void remove_instances(void)
 static void check_plugin(const char *plugin)
 {
 	char *buf;
+	char *str;
 	char *tok;
 
 	/*
@@ -2857,8 +2858,9 @@ static void check_plugin(const char *plugin)
 	if (!buf)
 		die("No plugins available");
 
-	while ((tok = strtok(buf, " "))) {
-		buf = NULL;
+	str = buf;
+	while ((tok = strtok(str, " "))) {
+		str = NULL;
 		if (strcmp(tok, plugin) == 0)
 			goto out;
 	}
