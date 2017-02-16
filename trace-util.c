ba9d5f844a7dc1b97ab96ca22b2df9245f052135
Julia Cartwright <julia@ni.com>
2017-02-16 16:57:21 -0600

    trace-cmd: Fixup more bogus symbol resolutions
    Commit ce1cbc51c ("trace-cmd: Leave out absolute addresses to fix bogus
    symbol resolutions") fixed up bogus symbol resolution for 'A' type
    symbols used on x86_64 for percpu offsets.
    
    However, this check did not cover per-cpu variables used in modules,
    which are generated as 'local' absolute symbols, and therefore appear as 'a' in
    /proc/kallsyms.  Fix this.
    
    Link: http://lkml.kernel.org/r/20170216225721.13087-1-julia@ni.com
    
    Cc: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Julia Cartwright <julia@ni.com>
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- trace-util.c
+++ trace-util.c
@@ -439,7 +439,7 @@ void parse_proc_kallsyms(struct pevent *pevent,
 		 *  - arm arch that adds a lot of bogus '$a' functions
 		 *  - x86-64 that reports per-cpu variable offsets as absolute
 		 */
-		if (func[0] != '$' && ch != 'A')
+		if (func[0] != '$' && ch != 'A' && ch != 'a')
 			pevent_register_function(pevent, func, addr, mod);
 		free(func);
 		free(mod);
